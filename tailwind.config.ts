c84e1f0632d24ace2f45b59489ea73cd063ed8ef
IvanOrsh <ksironov@mail.ru>
2024-01-15 23:50:20 +0300

    feat: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,22 @@
+import formsPlugin from "@tailwindcss/forms";
 import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
-import formsPlugin from "@tailwindcss/forms";
 
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["class", "[data-theme=dark]"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
         gray: colors.zinc,
+        myBlue: "#0A32B3",
+        myPink: "#BD365D",
       },
     },
   },
